11682d3feeb4d8604e8ec1316fa9aaabfa12a0d1
imnotharsha <harshadodderi@gmail.com>
2020-08-16 13:28:21 +0530

    Updates

---
+++ precache-manifest.d853972c1342803c79136ff0fe0ab023.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e07de43cbe7d85e226f0811074eb180",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "45cd5c3d6d60cd125eb7",
+    "url": "/portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "28a093b7f54f040cab31",
+    "url": "/portfolio/static/css/main.570ff73d.chunk.css"
+  },
+  {
+    "revision": "45cd5c3d6d60cd125eb7",
+    "url": "/portfolio/static/js/2.734ad3c6.chunk.js"
+  },
+  {
+    "revision": "4482739f3a8ff521b2955664b1fec844",
+    "url": "/portfolio/static/js/2.734ad3c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28a093b7f54f040cab31",
+    "url": "/portfolio/static/js/main.88add2aa.chunk.js"
+  },
+  {
+    "revision": "c4322a7adae57c8cb5cd",
+    "url": "/portfolio/static/js/runtime-main.dbf595e6.js"
+  }
+]);
